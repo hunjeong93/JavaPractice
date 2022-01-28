d6da2a7ccb22bdee7dbb07f1efcb4b86b9359d87
hunjeong93 <rhkrgnswjd@gmail.com>
2022-01-28 10:21:38 +0900

    서브마스터 브랜치 확인

--- src/main/java/chap12/Test1.java
+++ src/main/java/chap12/Test1.java
@@ -2,6 +2,7 @@ package chap12;
 
 public class Test1 {
 	public static void main(String[] args) {
-		System.out.println("깃허브 브랜치 생성후 변경 확인");
+		
+		System.out.println("깃허브 submaster 브랜치 푸쉬 확인");
 	}
 }
