abbce73ca855151b3cecceaf837cc1b1996ec024
hunjeong93 <rhkrgnswjd@naver.com>
2022-03-04 17:45:52 +0900

    잔디심기 테스트

--- src/main/java/chap15/Sample10.java
+++ src/main/java/chap15/Sample10.java
@@ -14,6 +14,6 @@ public class Sample10 {
 
 		System.out.println("["+ str1 + "] 한글이 포함되었나요? " + m1.find());
 		System.out.println("["+ str2 + "] 한글이 포함되었나요? " + m2.find());
-		//test
+		//git bash 아이디 변경
 	}
 }
